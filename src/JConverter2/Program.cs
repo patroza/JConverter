7909938d3b20a67730bad10afe10a16d9a00da1d
Patrick Roza <roza@sixnetworks.net>
2016-01-10 13:22:09 +0100

    jconv23

---
+++ src/JConverter2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace JConverter2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                foreach (var fn in args)
+                    ProcessFile(fn);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Press a key to exit");
+                Console.ReadKey();
+            }
+        }
+
+        private static void ProcessFile(string fn)
+        {
+            var outFn = fn + "-conv.dat";
+            if (File.Exists(outFn))
+                throw new NotSupportedException($"{outFn} already exists, please delete it first");
+
+            var data = File.ReadAllLines(fn);
+            var rx = new Regex(@"[\s\t]+");
+
+            foreach (var l in data.Select((x, i) => new {x, i}))
+            {
+                data[l.i] = rx.Replace(l.x, "\t").Trim('\t', ' ');
+            }
+
+            File.WriteAllText(outFn, string.Join(Environment.NewLine, data));
+        }
+    }
+}
