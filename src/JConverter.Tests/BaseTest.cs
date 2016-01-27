3e155df716a94a5b3a7565ecd7a2c61e1dbd3e47
Patrick Roza <roza@sixnetworks.net>
2016-01-27 09:26:05 +0100

    Add base test

---
+++ src/JConverter.Tests/BaseTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoFakeItEasy;
+
+namespace JConverter.Tests
+{
+    [TestFixture]
+    public abstract class BaseTest
+    {
+        protected Fixture Fixturer { get; } = new Fixture();
+
+        protected BaseTest()
+        {
+            Fixturer.Customize(new AutoFakeItEasyCustomization());
+        }
+    }
+}
